7c8fc2d09a40b52184f577156273364f6abdebea
Abu Bokor <abubokor1066@gmail.com>
2023-05-02 17:06:49 +0600

    banner added

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import banner2 from "../..//assets/pexels1-chan-walrus-958545.jpg";
 
 const Home = () => {
   return (
     <div>
-      <h2>This is a home</h2>
+      <div className=" bg-black">
+        <img className="" src={banner2} alt="" />
+      </div>
+      <div className="my-10">
+        <h2 className="text-3xl font-semibold text-center">Our Best Chef</h2>
+      </div>
     </div>
   );
 };
